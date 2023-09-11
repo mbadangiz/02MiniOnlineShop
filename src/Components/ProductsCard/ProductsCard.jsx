23a514119952626a634dbd2ad90e1ca897ff300e
Mohammad Badangiz <mbz3a1998@gmail.com>
2023-09-11 19:33:14 +0330

    product detail styles is done.

--- src/Components/ProductsCard/ProductsCard.jsx
+++ src/Components/ProductsCard/ProductsCard.jsx
@@ -4,31 +4,37 @@ import Button from "../Common/Button/Button";
 const ProductsCard = () => {
   return (
     <div className="w-[400px] h-60 bg-white rounded-lg px-5 py-3  box-shadow-card flex flex-row items-center content-center  ">
-      <img src="./../Img/Img-Products.jpg" className="h-40" alt="" />
-      <div className="flex flex-col flex-wrap h-full justify-between text-sm">
-        <h2 className="f-bold">لباس بچگانه تابستانه مارک فلان چی</h2>
-        <div className="w-max text-xs rounded-2xl text-gray-600 bg-gray-100 py-1 px-4 ">
-          لباس کودک
-        </div>
-        <p className="f-bold text-base  text-emerald-500 ">
-          <span className="line-through text-gray-300 ml-1 text-xs ">
-            2980000 ریال
-          </span>
-
-          <span className="text-base mr-1 ">2000000 ریال</span>
-        </p>
-
-        <p className="text-xs w-max text-white bg-rose-600 py-[2px] px-2 rounded-2xl">
-          30%
-        </p>
-        <Link to="/productDetail?pId=mamad">
-          <Button
-            data={{ type: "button", text: "مشاهده ی جزئیات", size: "small" }}
-          />
-        </Link>
-      </div>
+      <img src="./../Img/Img-Products.jpg" className="w-32" alt="" />
+      <ProductCardBody />
     </div>
   );
 };
 
 export default ProductsCard;
+
+const ProductCardBody = () => {
+  return (
+    <div className="flex flex-col flex-wrap h-full justify-between text-sm">
+      <h2 className="f-bold">لباس بچگانه تابستانه مارک فلان چی</h2>
+      <div className="w-max text-xs rounded-2xl text-gray-600 bg-gray-100 py-1 px-4 ">
+        لباس کودک
+      </div>
+      <p className="f-bold text-base  text-emerald-500 ">
+        <span className="line-through text-gray-300 ml-1 text-xs ">
+          2980000 ریال
+        </span>
+
+        <span className="text-base mr-1 ">2000000 ریال</span>
+      </p>
+
+      <p className="text-xs w-max text-white bg-rose-600 py-[2px] px-2 rounded-2xl">
+        30%
+      </p>
+      <Link to="/productDetail?pId=mamad">
+        <Button
+          data={{ type: "button", text: "مشاهده ی جزئیات", size: "small" }}
+        />
+      </Link>
+    </div>
+  );
+};
--- src/Components/ProductsDetail/ProductsDetail.jsx
+++ src/Components/ProductsDetail/ProductsDetail.jsx
@@ -2,8 +2,44 @@ import { useSearchParams } from "react-router-dom";
 
 const ProductsDetail = () => {
   const [queryString] = useSearchParams();
-  console.log(Number(queryString.get("pId")));
-  return <div>ProductsDetail</div>;
+  // console.log(Number(queryString.get("pId")));
+  return (
+    <div
+      className="w-1280max h-[calc(100vh-60px)] mx-auto pt-10 flex flex-row flex-wrap 
+      justify-center items-center content-center gap-10"
+    >
+      <img src="./../Img/Img-Products.jpg" className="w-[400px]" alt="" />
+      <ProductsDetailBody />
+    </div>
+  );
 };
 
 export default ProductsDetail;
+
+const ProductsDetailBody = () => {
+  return (
+    <div className="w-[400px] space-y-5">
+      <h1 className="f-bold text-xl">لباس بچه کانه</h1>
+      <div className="w-max text-xs rounded-2xl text-gray-600 bg-gray-200 py-1 px-4 ">
+        لباس کودک
+      </div>
+      <p className="text-justify text-sky-950/50 ">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
+        از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
+        متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه
+        درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را میکند.
+      </p>
+
+      <p className="f-bold text-base  text-emerald-500 ">
+        <span className="line-through text-gray-300 ml-1 text-base ">
+          2980000 ریال
+        </span>
+        <span className="text-base mr-1 ">2000000 ریال</span>
+      </p>
+      <p className="text-xs w-max text-white bg-rose-600 py-[2px] px-2 rounded-2xl">
+        30%
+      </p>
+    </div>
+  );
+};
